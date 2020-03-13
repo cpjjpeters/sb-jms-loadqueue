f3bef7ab525d33ff4888feadaf23afc3928bc06a
cpjjpeters <cpjjpeters@gmail.com>
2020-03-13 16:52:19 +0100

    initial

---
+++ src/test/java/be/belfius/springjmsloadqueue/SbJmsLoadqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package be.belfius.springjmsloadqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbJmsLoadqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
